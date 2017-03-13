ad70371671af34d588a70aae40a4545d1dd63239
PC-201703031236\Win 10 Pro x64 <phuongta@cheapgo.com>
2017-03-13 17:38:42 +0700

    implement document of driver

---
+++ src/app/modules/vehicle-document/vehicle-document.controller.js
@@ -0,0 +1,13 @@
+/**
+ * Author : phuong.tran
+ * Date   : 2017-03-13
+ */
+
+(function () {
+  'use strict';
+  angular.module('app').controller('vehicle-document',controller);
+
+  function controller() {
+      
+  }
+})();
